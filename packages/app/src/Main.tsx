595a95dfc3366a42ea46028658559a3628862c6c
Ravi Teja Prathi <raviteja.prathi@qapitacorp.com>
2022-11-10 20:14:25 +0530

    Fixed a build error

--- packages/app/src/Main.tsx
+++ packages/app/src/Main.tsx
@@ -34,7 +34,6 @@ const Main = () => {
                     <Route path="/surrenders" element={<System system={{ remote: 'liquidity', url: (window as any).liquidityMFE, module: './LiquidityApp'  }} />} />
                     <Route path="/approvals" element={<System system={{ remote: 'approval', url: (window as any).approvalMFE, module: './ApprovalApp'  }} />} />
                     <Route path="*" element={"No match found"}/>
-                    <Route path="/" element={<HomePage />} />
                   </Routes>     
             </main>
         </div>
